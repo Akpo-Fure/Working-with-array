a9e3b05cf2c2d2f27d2bb0a8ad4adac8e2595d57
Akpo_Fure <okegbeakpofurekelvin@gmail.com>
2023-07-10 23:56:52 +0100

    Looping Arrays forEach()

--- script.js
+++ script.js
@@ -106,11 +106,31 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 
 
-//The new at Method
-const arr = [11, 45, 78]
-console.log(arr[0])
-console.log(arr.at(0))
-//getting last array element
-console.log(arr[arr.length- 1])
-console.log(arr.slice(-1)[0])
-console.log(arr.at(-1))
+// //The new at Method
+// const arr = [11, 45, 78]
+// console.log(arr[0])
+// console.log(arr.at(0))
+// //getting last array element
+// console.log(arr[arr.length- 1])
+// console.log(arr.slice(-1)[0])
+// console.log(arr.at(-1))
+
+
+
+// //Looping Arrays_forEach
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for(const [i, movement] of movements.entries() ){
+//   if(movement > 0){
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`)
+//   }else{
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// }
+// console.log("==========FOREACH==========")
+// movements.forEach(function(movement, i, arr){
+//   if(movement > 0){
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`)
+//   }else{
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// })
